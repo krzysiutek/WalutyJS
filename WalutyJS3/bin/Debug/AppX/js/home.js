a000e0248ada54a36898f4ffc67aed97d678aad5
Krzysztof Barański <krzysiek0516@interia.eu>
2016-08-31 22:54:07 +0200

    Initial commit. Selecting dates, download and parse XML

---
+++ WalutyJS3/bin/Debug/AppX/js/home.js
@@ -0,0 +1,153 @@
+﻿'use strict';
+
+(function () {
+
+    var httpClient;
+    var BASE_DATES_URL = "http://www.nbp.pl/kursy/xml/dir.txt";
+    var dates = [];
+    var xmlNames = [];
+    var dateSelect;
+    var XML_FILE_NAME = "xmlData.txt";
+    var currencyList = [];
+
+    checkConnection = function () {
+        console.log("NAVIGATOR!!!!!!" + navigator.onLine);
+        return navigator.onLine;
+    }
+
+    function getDates() {
+        httpClient = new XMLHttpRequest();
+        httpClient.open('GET', BASE_DATES_URL);
+        httpClient.onreadystatechange = getDatesCallback;
+        httpClient.send();
+    }
+
+    function getDatesCallback() {
+        var response = httpClient.responseText;
+        if (httpClient.readyState === 4) {
+            var splittedResponseByLine = response.split('\n');
+
+            getOnlyItemsWithA(splittedResponseByLine);
+
+            fillDateSelect();
+
+        }
+    }
+
+    function onDateChange (data) {
+        var selectedItemValue = dateSelect.value;
+        getXmlFile(selectedItemValue).then(function (data) {
+            var fileContent = data;
+            if (!fileContent) {
+                downloadXml(selectedItemValue);
+            } else {
+                parseXmlFile(fileContent);
+            }
+        });
+    }
+
+    function downloadXml(xmlName) {
+        var watingText = document.getElementById('waitingText').style.visibility = 'visible';
+        var xmlUrl = "http://www.nbp.pl/kursy/xml/" + xmlName;
+        var xmlData;
+        var i;
+
+        httpClient = new XMLHttpRequest();
+        httpClient.open('GET', xmlUrl, false);
+        httpClient.send();
+
+        xmlData = httpClient.responseXML;
+        
+        saveXmlFile(xmlData.getElementsByTagName('kursy_walut'), xmlName);
+
+        parseXmlFile(xmlData);
+    }
+
+    function getXmlFile(fileName) {
+        var that = this;
+        return Windows.Storage.ApplicationData.current.localFolder.getFileAsync(fileName).then(function (file) {
+            return Windows.Storage.FileIO.readTextAsync(file).then(function (fileContent) {
+                return fileContent;
+            },
+            function (error) {
+                console.log("Błąd odczytu");
+            });
+        },
+        function (error) {
+            console.log("Nie znaleziono pliku");
+        });
+    }
+
+    function parseXmlFile(data) {
+        var currencyObject = {};
+        var currencies;
+        var i;
+        currencies = data.getElementsByTagName('pozycja');
+
+        for (i = 0; i < currencies.length; i++) {
+            currencyObject = {
+                name: currencies[i].getElementsByTagName('nazwa_waluty')[0].childNodes[0].nodeValue,
+                code: currencies[i].getElementsByTagName('kod_waluty')[0].childNodes[0].nodeValue,
+                conversionRate: currencies[i].getElementsByTagName('przelicznik')[0].childNodes[0].nodeValue,
+                exchangeRate: currencies[i].getElementsByTagName('kurs_sredni')[0].childNodes[0].nodeValue
+            }
+            currencyList.push(currencyObject);
+        }
+    }
+
+    function saveXmlFile(data, fileName) {
+        var applicationData = Windows.Storage.ApplicationData.current;
+        var localFolder = applicationData.localFolder;
+        localFolder.createFileAsync(fileName, Windows.Storage.CreationCollisionOption.replaceExisting)
+            .then(function (file) {
+                return Windows.Storage.FileIO.writeTextAsync(file, data);
+            }).done(function () {
+        });
+    }
+
+    function fillDateSelect() {
+        var i;
+
+        dateSelect = document.getElementById("date-select");
+        dateSelect.addEventListener('change', onDateChange);
+
+        //dateSelect.options = []; /* will no works :( */ 
+        dateSelect.options.length = 0; // clear options array 
+
+        for (i = 0; i < dates.length; i++) {
+            var option = document.createElement('option');
+            
+            option.value = xmlNames[i];
+            option.text = dates[i];
+
+            dateSelect.appendChild(option);
+        }
+    }
+
+    function getOnlyItemsWithA(items) {
+        var i, j = 0;
+        for (i = 0; i < items.length; i++) {
+            if (items[i].substring(0, 1) === 'a') {
+                dates[j] = '20' + items[i].substring(5, 7) +
+                    '-' + items[i].substring(7, 9) + '-' + items[i].substring(9, 11); // make proper date (eg. 2016-02-01)
+                xmlNames[j] = items[i].substring(0, 11) + '.xml'; // remove EOL sign
+                j++;
+            }
+        }
+        dates.reverse(); // reverse array to set last date on top
+        xmlNames.reverse();
+    }
+
+  
+
+    WinJS.UI.Pages.define("/pages/home.html", {
+        ready: function (element, options) {
+    
+            if (checkConnection()) {
+                getDates();
+            }
+        }
+    });
+
+    WinJS.UI.processAll();
+})();
---
+++ WalutyJS3/js/main.js
@@ -0,0 +1,61 @@
+﻿var checkConnection;
+(function () {
+  'use strict';
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    var nav = WinJS.Navigation
+
+    
+
+  app.onactivated = function (args) {
+    if (args.detail.kind === activation.ActivationKind.launch) {
+      if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+        // TODO: This application has been newly launched. Initialize your application here.
+      } else {
+        // TODO: This application has been reactivated from suspension.
+        // Restore application state here.
+      }
+      args.setPromise(WinJS.UI.processAll().then(function() {
+          // TODO: Your code here.
+          
+          
+          var itemArray = [
+                { title: "Marvelous Mint", text: "Gelato", picture: "/images/fruits/60Mint.png" },
+                { title: "Succulent Strawberry", text: "Sorbet", picture: "/images/fruits/60Strawberry.png" },
+                { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+                { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" },
+                { title: "Creamy Orange", text: "Sorbet", picture: "/images/fruits/60Orange.png" },
+                { title: "Very Vanilla", text: "Ice Cream", picture: "/images/fruits/60Vanilla.png" },
+                { title: "Banana Blast", text: "Low-fat frozen yogurt", picture: "/images/fruits/60Banana.png" },
+                { title: "Lavish Lemon Ice", text: "Sorbet", picture: "/images/fruits/60Lemon.png" }
+          ];
+
+          var items = [];
+
+          // Generate 160 items
+          for (var i = 0; i < 20; i++) {
+              itemArray.forEach(function (item) {
+                  items.push(item);
+              });
+          }
+
+
+          WinJS.Namespace.define("Sample.ListView", {
+              data: new WinJS.Binding.List(items)
+          });
+
+          WinJS.UI.processAll();
+
+
+          return nav.navigate(Application.navigator.home);
+      }));
+    }
+  };
+  app.oncheckpoint = function (args) {
+    // TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
+    // You might use the WinJS.Application.sessionState object, which is automatically saved and restored across suspension.
+    // If you need to complete an asynchronous operation before your application is suspended, call args.setPromise().
+  };
+  app.start();
+}());
